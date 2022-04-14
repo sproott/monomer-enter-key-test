4a0109f93f40ce5ece1280d07de1c9837c12b13a
sproott <david.hrabe12@seznam.cz>
2022-04-14 22:43:20 +0200

    Init example

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
@@ -7,55 +6,49 @@ import Control.Lens
 import Data.Maybe
 import Data.Text (Text)
 import Monomer
+import qualified Monomer.Lens as L
 import TextShow
 
-import qualified Monomer.Lens as L
+type AppModel = ()
 
-newtype AppModel = AppModel {
-  _clickCount :: Int
-} deriving (Eq, Show)
-
-data AppEvent
-  = AppInit
-  | AppIncrease
+data AppEvent = AENoOp | AEEnter | AEButton
   deriving (Eq, Show)
 
-makeLenses 'AppModel
+buildUI ::
+  WidgetEnv AppModel AppEvent ->
+  AppModel ->
+  WidgetNode AppModel AppEvent
+buildUI wenv model = widgetTree
+  where
+    widgetTree = keystroke [("Enter", AEEnter)] $ button "Press me" AEButton
 
-buildUI
-  :: WidgetEnv AppModel AppEvent
-  -> AppModel
-  -> WidgetNode AppModel AppEvent
-buildUI wenv model = widgetTree where
-  widgetTree = vstack [
-      label "Hello world",
-      spacer,
-      hstack [
-        label $ "Click count: " <> showt (model ^. clickCount),
-        spacer,
-        button "Increase count" AppIncrease
-      ]
-    ] `styleBasic` [padding 10]
-
-handleEvent
-  :: WidgetEnv AppModel AppEvent
-  -> WidgetNode AppModel AppEvent
-  -> AppModel
-  -> AppEvent
-  -> [AppEventResponse AppModel AppEvent]
+handleEvent ::
+  WidgetEnv AppModel AppEvent ->
+  WidgetNode AppModel AppEvent ->
+  AppModel ->
+  AppEvent ->
+  [AppEventResponse AppModel AppEvent]
 handleEvent wenv node model evt = case evt of
-  AppInit -> []
-  AppIncrease -> [Model (model & clickCount +~ 1)]
+  AENoOp -> []
+  AEEnter ->
+    [ Task $ do
+        putStrLn "Enter pressed"
+        pure AENoOp
+    ]
+  AEButton ->
+    [ Task $ do
+        putStrLn "Button pressed"
+        pure AENoOp
+    ]
 
 main :: IO ()
 main = do
   startApp model handleEvent buildUI config
   where
-    config = [
-      appWindowTitle "Hello world",
-      appWindowIcon "./assets/images/icon.bmp",
-      appTheme darkTheme,
-      appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf",
-      appInitEvent AppInit
+    config =
+      [ appWindowTitle "Hello world",
+        appWindowIcon "./assets/images/icon.bmp",
+        appTheme darkTheme,
+        appFontDef "Regular" "./assets/fonts/Roboto-Regular.ttf"
       ]
-    model = AppModel 0
+    model = ()
